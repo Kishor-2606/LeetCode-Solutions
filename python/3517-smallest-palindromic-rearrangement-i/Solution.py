06f5711000f780b984b081674006c5370933f4f1
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-07-28 21:53:50 +0530

    Add/update solution: Smallest Palindromic Rearrangement I

--- python/3517-smallest-palindromic-rearrangement-i/Solution.py
+++ python/3517-smallest-palindromic-rearrangement-i/Solution.py
@@ -1,9 +1,23 @@
-
 class Solution(object):
     def smallestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+        freq=[0]*26
+        for c in s:
+            freq[ord(c)-ord('a')]+=1
 
-        # if len(s)>1 and len(s)%2==0:
+        n=len(s)
+        ans=['']*n
+
+        left,right=0,n-1
+
+        for i in range(26):
+            while freq[i]>=2:
+                ans[left]=chr(ord('a')+i)
+                ans[right]=chr(ord('a')+i)
+                left+=1
+                right-=1
+                freq[i]-=2
+            if freq[i]==1:
+                ans[n//2]=chr(ord('a')+i)
+
+        return ''.join(ans)
+        
