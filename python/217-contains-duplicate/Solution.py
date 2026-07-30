f99b51799f31d79078b10c97ce159ba152052d8a
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-07-30 20:42:44 +0530

    Add/update solution: Contains Duplicate

--- python/217-contains-duplicate/Solution.py
+++ python/217-contains-duplicate/Solution.py
@@ -1,8 +1,4 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        seen=set()
-        for i in nums:
-            if i in seen:
-                return True
-            seen.add(i)
-        return False
+        seen=set(nums)
+        return len(seen)!=len(nums)
