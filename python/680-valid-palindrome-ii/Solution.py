1d14d657ae11070def079ccaf962c267f5893cab
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-07-30 19:19:55 +0530

    Add/update solution: Valid Palindrome II

--- python/680-valid-palindrome-ii/Solution.py
+++ python/680-valid-palindrome-ii/Solution.py
@@ -6,32 +6,17 @@ class Solution(object):
             st=st+1
             en=en-1
         return True
-
     def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        if s=="":
-            return False
-        elif len(s)==1:
-            return True
-        elif len(s)==2:
-            return True
-        elif len(s)==3:
-            return s[0]==s[2]
-        else:
-            i=0
-            j=len(s)-1
-            while(i<j):
-                if s[i]==s[j]:
-                    pass
+        i=0
+        j=len(s)-1
+        while(i<j):
+            if s[i]==s[j]:
+                pass
+            else:
+                if(self.is_palindrome(s,i+1,j) or self.is_palindrome(s,i,j-1)):
+                    return True
                 else:
-                    if(self.is_palindrome(s,i+1,j) or self.is_palindrome(s,i,j-1)):
-                        return True
-                    else:
-                        return False
-                i=i+1
-                j=j-1
-            return True
-                    
+                    return False
+            i=i+1
+            j=j-1
+        return True
