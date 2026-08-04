89cb630edf9df4db72bc45eada06615862b5ff8d
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-08-04 09:02:26 +0530

    Add/update solution: Longest Harmonious Subsequence

---
+++ python/594-longest-harmonious-subsequence/Solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findLHS(self, nums):
+        freq={}
+        for i in nums:
+            freq[i]=freq.get(i,0)+1
+        mx=0
+        for key in freq.keys():
+            if freq.get(key+1,0):
+                mx=max(mx,freq[key]+freq[key+1])
+        return mx
+            
+        
