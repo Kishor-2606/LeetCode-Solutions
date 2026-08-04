30a1f5a9dcaad103e0aac655bedbc0651d83d71e
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-08-04 07:51:42 +0530

    Add/update solution: Find Missing Elements

--- python/3731-find-missing-elements/Solution.py
+++ python/3731-find-missing-elements/Solution.py
@@ -2,7 +2,7 @@ class Solution(object):
     def findMissingElements(self, nums):
         st=[]
         m,n=min(nums),max(nums)
-        nums=set(sorted(nums))
+        nums=set(nums)
         for i in range(m+1,n):
             if i not in nums:
                 st.append(i)
