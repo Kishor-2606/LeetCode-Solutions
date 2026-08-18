be4d1bac3c00a6362c7659d3f8fa1e9ca7b40bab
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-08-18 22:31:27 +0530

    Add/update solution: Find the Largest Almost Missing Integer

--- python/3471-find-the-largest-almost-missing-integer/Solution.py
+++ python/3471-find-the-largest-almost-missing-integer/Solution.py
@@ -1,27 +1,30 @@
 class Solution(object):
     def largestInteger(self, nums, k):
-        seen=set()
         freq={}
-        i=0
-        j=0
-        cnt=0
-        while(j<len(nums)):
-            if cnt==k:
-                i=i+1
-                j=i-1
-                cnt=-1
-                seen.clear()
-            elif nums[j] not in seen:
-                freq[nums[j]]=freq.get(nums[j],0)+1
-                seen.add(nums[j])
-            j=j+1
-            cnt+=1
+        window_freq={}
+        l=0
+        r=k
+        for i in nums[:k]:
+            window_freq[i]=window_freq.get(i,0)+1
+        
+        for i in window_freq:
+            freq[i]=freq.get(i,0)+1
+        
+        while(r<len(nums)):
+            print(window_freq)
+            window_freq[nums[l]]-=1
+            window_freq[nums[r]]=window_freq.get(nums[r],0)+1
+            if window_freq[nums[l]]==0:
+                del window_freq[nums[l]]
+            for i in window_freq:
+                freq[i]=freq.get(i,0)+1
+            l+=1
+            r+=1 
+
         mx=-1
-        mn=10000
         for key,value in freq.items():
             if value==1 and mx<key:
                 mx=key
-                mn=value
         return mx
 
         
