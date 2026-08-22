4e7a12dda2e4223c3c074799568f1dae211d347f
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-08-22 12:55:15 +0530

    Add/update solution: Count Substrings That Satisfy K-Constraint I

--- python/3258-count-substrings-that-satisfy-k-constraint-i/Solution.py
+++ python/3258-count-substrings-that-satisfy-k-constraint-i/Solution.py
@@ -3,17 +3,22 @@ class Solution(object):
         freq={}
         l,r=0,0
         cnt=0
-        while(l<len(s)):
-            if l!=r:
-                cnt+=1
-            if r<len(s):
+        memory=100
+        while(l<len(s) and r<len(s)):
+            if r<len(s) and r!=memory:
+                memory=r
                 freq[s[r]]=freq.get(s[r],0)+1
-            if (freq.get('1',0)<=k or freq.get('0',0)<=k) and r<len(s):
+            if (freq.get('1',0)<=k or freq.get('0',0)<=k) and r<len(s)-1:
                 r=r+1
-            elif (freq.get('1',0)>k and freq.get('0',0)>k) or (r==len(s)):
-                freq.clear()
+            elif (freq.get('1',0)>k and freq.get('0',0)>k):
+                cnt+=freq.get('0',0)+freq.get('1',0)-1
+                freq[s[l]]-=1
                 l=l+1
-                r=l    
+            else:
+                if (r==len(s)-1):
+                    cnt+=freq.get('0',0)+freq.get('1',0)
+                    freq[s[l]]-=1
+                    l=l+1
         return cnt
             
 
