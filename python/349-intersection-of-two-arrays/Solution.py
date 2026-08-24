233bee598c231222dab449d3b0f9040c7f407762
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-08-24 23:00:00 +0530

    Add/update solution: Intersection of Two Arrays

--- python/349-intersection-of-two-arrays/Solution.py
+++ python/349-intersection-of-two-arrays/Solution.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
         st=set()
+        nums1=set(nums1)
+        nums2=set(nums2)
         for i in nums1:
             if i in nums2:
                 st.add(i)
