33469b334d9787c1f0144d159622bd9b60522c0e
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-08-27 20:36:17 +0530

    Add/update solution: Arranging Coins

--- python/441-arranging-coins/Solution.py
+++ python/441-arranging-coins/Solution.py
@@ -5,14 +5,10 @@ class Solution(object):
         answer=1
         while start<=end:
             middle=(start+end)//2
-            print(middle)
             coins=middle*(middle+1)//2
-            print(coins)
-            print(start,end)
             if coins>n:
                 end=middle-1
             elif coins<=n:
                 answer=middle
                 start=middle+1
-            print(answer)
         return answer
