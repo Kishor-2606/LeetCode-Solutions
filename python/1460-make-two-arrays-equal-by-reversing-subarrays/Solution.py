12fcc9d712172734c5c21de1ff6f2147ce4fb8bb
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-08-29 20:45:04 +0530

    Add/update solution: Make Two Arrays Equal by Reversing Subarrays

--- python/1460-make-two-arrays-equal-by-reversing-subarrays/Solution.py
+++ python/1460-make-two-arrays-equal-by-reversing-subarrays/Solution.py
@@ -1,16 +1,8 @@
 class Solution(object):
-    def canBeEqual(self, target, arr):
+    def canBeEqual(self, target, arr): 
         freq={}
         for i,j in zip(target,arr):
             freq[i]=freq.get(i,0)+1
             freq[j]=freq.get(j,0)-1
-            
-        for value in freq.values():
-            if value!=0:
-                return False
-        return True
 
-        # arr=sorted(arr)
-        # target=sorted(target)
-        # return arr==target
-        
+        return all(v==0 for v in freq.values())
