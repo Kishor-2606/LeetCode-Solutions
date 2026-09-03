b5c079cf36fe57348ced3d5e3a95761e91c17d6d
Kishor Kumar <kishorkumarss2612@gmail.com>
2026-09-03 21:36:39 +0530

    Add/update solution: Construct Uniform Parity Array II

--- python/3876-construct-uniform-parity-array-ii/Solution.py
+++ python/3876-construct-uniform-parity-array-ii/Solution.py
@@ -1,23 +1,6 @@
 class Solution(object):
     def uniformArray(self,nums1):
-        odd=0
-        even=0
-
-        for i in nums1:
-            if i%2==0:
-                even+=1
-            else:
-                odd+=1
-
-        if odd==0 or even==0:
+        min_val = min(nums1)
+        if min_val % 2 != 0:
             return True
-
-        smallest_odd=float('inf')
-
-        for i in nums1:
-            if i%2!=0:
-                smallest_odd=min(smallest_odd,i)
-        for i in nums1:
-            if i%2==0 and i<smallest_odd:
-                return False
-        return True
+        return all(x % 2 == 0 for x in nums1)
